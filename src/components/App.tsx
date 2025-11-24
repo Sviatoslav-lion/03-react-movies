7ce71991d85e8838407e0d8000f6766d09cecf00
Sviatoslav <sviat.danko@gmail.com>
2025-11-24 14:23:27 +0200

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+// src/components/App.tsx
+
+import Product from './product/Product';
+
+export default function App() {
+  return (
+    <>
+      <h1>Best selling</h1>
+      <Product name="Tacos With Lime" />
+      <Product name="Fries and Burger" />
+    </>
+  );
+}
