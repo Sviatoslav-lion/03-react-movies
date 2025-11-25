fa47ef4a544cc1dd556de61f1a67e31870e1f81b
Sviatoslav <sviat.danko@gmail.com>
2025-11-25 07:02:22 +0000

    UPD create folder App

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // Нормалізація стилів
 import 'modern-normalize/modern-normalize.css';
-import App from "./components/App";
+import App from "./components/App/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
